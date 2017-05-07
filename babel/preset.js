69bc57ce0c60beee3f4363de49194ccdfc4d5c29
Linus Miller <lohfu@lohfu.io>
2017-05-07 21:05:37 +0200

    Fix last update

--- babel/preset.js
+++ babel/preset.js
@@ -7,4 +7,4 @@ var plugins = require( baseLocation ).plugins.slice();
 
 var commonjsPlugin = relative( 'babel-plugin-transform-es2015-modules-commonjs', baseLocation );
 
-module.exports = { plugins: plugins.filter((plugin) => plugin !== commonjsPlugin) };
+module.exports = { plugins: plugins.filter((plugin) => plugin !== commonjsPlugin && plugin[0] !== commonjsPlugin) };
